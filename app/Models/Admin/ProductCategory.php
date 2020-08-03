a3c3a0f59707195c23815c99163ad37ef9ed20f7
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2020-08-04 00:07:25 +0600

    Update Code

--- app/Models/Admin/ProductCategory.php
+++ app/Models/Admin/ProductCategory.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductCategory extends Model {
+    use SoftDeletes;
     protected $guarded = [];
 
     public function getStatusTextAttribute() {
